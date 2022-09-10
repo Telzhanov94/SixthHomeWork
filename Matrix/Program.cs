799c08528c95ec8bfcc211c05396e5f128b4445f
Temirlan <telzhanov94@gmail.com>
2022-09-10 10:26:08 +0600

    Done

---
+++ Matrix/Program.cs
@@ -0,0 +1,30 @@
+﻿void FillArray(int [,] array)
+{
+    for(int i=0;i<array.GetLength(0);i++)
+    {
+        for(int j=0;j<array.GetLength(1);j++)
+        array[i,j]=new Random().Next(-100,100);
+    }
+}
+void PrintArray(int [,] array)
+{
+    for(int i=0;i<array.GetLength(0);i++)
+    {
+        for(int j=0;j<array.GetLength(1);j++)
+        Console.Write($"{array[i,j]} \t");
+        Console.WriteLine();
+    }
+}
+int [,] Matrix(int number, int digit)
+{
+    int [,] NewMatrix=new int [number,digit];
+    return NewMatrix;
+}
+Console.WriteLine("Enter the number of lines");
+int lines = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Enter the number of columns");
+int columns = int.Parse(Console.ReadLine()??"0");
+int [,] Matr=Matrix(lines,columns);
+PrintArray(Matr);
+FillArray(Matr);
+PrintArray(Matr);
